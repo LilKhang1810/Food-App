baee20c90871ec10be9e614296df0f349dd8434f
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2024-02-01 11:39:51 +0700

    update func "Cập nhật giá tiền"

--- FoodApp/View/DetailDishesView.swift
+++ FoodApp/View/DetailDishesView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 import SDWebImageSwiftUI
 struct DetailDishesView: View {
     @State var product: Product
-    @State private var selectedSize: String = "S"
-    @State private var previousSize: String = "S"
-    @StateObject var burgerOption = BurgerOptionViewModel()
-    @StateObject var pizzaOption = PizzaOptionViewModel()
     @StateObject var productVM = ProductViewController()
     @State var showPizzaOptionScreen:Bool = false
     @State var showBurgerOptionScreen:Bool = false
-    @State var selectedPizzaOption: String = ""
-    @State var selectedBurgerOption: String = ""
+    @State var selectedPizzaOption: String = "No option"
+    @State var selectedBurgerOption: String = "No option"
+    @State var quantity: Int = 1
+    @State var optionPrice: Int = 0
+    var subTotal: Int {
+        return (product.price + optionPrice) * quantity
+    }
     let selectedType: String
     var filterType:[Product] {
         productVM.products.filter{$0.type == selectedType}
@@ -42,7 +43,6 @@ struct DetailDishesView: View {
                                                 .weight(.medium)
                                         )
                                         .foregroundColor(Color(red: 0.97, green: 0.6, blue: 0.16))
-                                    Text("\(product.price)đ")
                                 }
                                 Spacer()
                                 VStack(alignment:.trailing){
@@ -58,7 +58,6 @@ struct DetailDishesView: View {
                         }
                         
                         .padding(.horizontal,10)
-                        Text("\(selectedType)")
                         if selectedType == "pizza" {
                             VStack{
                                 Button(action: { showPizzaOptionScreen.toggle()
@@ -89,14 +88,69 @@ struct DetailDishesView: View {
                                     }
                             }
                         }
-                        if selectedType == "chicken fried"{
-                            Text("not things")
+                        HStack{
+                            VStack(alignment:.leading){
+                                Text("Quantity")
+                                    .font(Font.custom("Bebas Neue", size: 15))
+                                    .foregroundColor(Color("AccentColor"))
+                                Rectangle()
+                                    .foregroundColor(.clear)
+                                    .frame(width: 157, height: 40)
+                                    .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                                
+                                    .cornerRadius(20)
+                                    .overlay{
+                                        HStack{
+                                            Text("\(quantity)")
+                                            Spacer()
+                                            Button(
+                                                action: {
+                                                    if quantity == 1{
+                                                        quantity = 1
+                                                    }
+                                                    else{
+                                                        quantity -= 1
+                                                    }
+                                                }) {
+                                                    Image(systemName: "minus")
+                                                }
+                                                .padding(.trailing,10)
+                                            
+                                            Button(action: {quantity+=1}) {
+                                                Image(systemName: "plus")
+                                            }
+                                        }
+                                        .padding(.horizontal,20)
+                                    }
+                            }
+                            Spacer()
+                            VStack(alignment: .trailing){
+                                Text("Sub total")
+                                    .font(Font.custom("Bebas Neue", size: 15))
+                                Text("\(subTotal)đ")
+                                    .font(Font.custom("Bebas Neue", size: 25))
+                                    .foregroundColor(Color("AccentColor"))
+                            }
                         }
+                        .padding(.horizontal,20)
+                        Button(
+                            action: {print("\(optionPrice)")},
+                            label: {
+                                Rectangle()
+                                    .foregroundColor(Color("AccentColor"))
+                                    .frame(width: 320,height: 50)
+                                    .cornerRadius(25)
+                                    .overlay{
+                                        Text("Add to basket")
+                                            .font(Font.custom("Bebas Neue", size: 25))
+                                            .foregroundColor(.white)
+                                    }
+                            })
                     }
                 }
                 if showPizzaOptionScreen == true{
                     
-                    optionPizzaScreen(showPizzaOptionScreen: $showPizzaOptionScreen, selectedPizzaOption: $selectedPizzaOption) { option in
+                    optionPizzaScreen(showPizzaOptionScreen: $showPizzaOptionScreen, selectedPizzaOption: $selectedPizzaOption,optionPrice: $optionPrice) { option in
                         selectedPizzaOption = option
                         showPizzaOptionScreen = false
                     }
@@ -106,7 +160,7 @@ struct DetailDishesView: View {
                 
                 if showBurgerOptionScreen{
                     
-                    optionBurgerScreen(showBurgerOptionScreen: $showBurgerOptionScreen, selectedBurgerOption: $selectedBurgerOption) { option in
+                    optionBurgerScreen(showBurgerOptionScreen: $showBurgerOptionScreen, selectedBurgerOption: $selectedBurgerOption,optionPrice: $optionPrice) { option in
                         selectedBurgerOption = option
                         showBurgerOptionScreen = false
                     }
@@ -120,14 +174,14 @@ struct DetailDishesView: View {
 
 struct DetailDishesView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailDishesView(product: Product(id: "1PI3RayUQuHXOnxGVEcE", name: "EVM serving of 6 chicken McNuggets™", price: 79000, img_url: "https://mcdonalds.vn/uploads/2018/food/evm/EVM_03_6pcs_chicken_mcnuggets_0.png", brand: "Mc Donald", popular: true,type: "chicken fried"), selectedType: "hamburger")
+        DetailDishesView(product: Product(id: "1PI3RayUQuHXOnxGVEcE", name: "EVM serving of 6 chicken McNuggets™", price: 79000, img_url: "https://mcdonalds.vn/uploads/2018/food/evm/EVM_03_6pcs_chicken_mcnuggets_0.png", brand: "Mc Donald", popular: true,type: "chicken fried"), selectedType: "pizza")
     }
 }
 
 struct optionPizzaScreen:View{
-    let pizzaOption = ["Add meat","Add cheese","Upgrade Size M","Upgrade Size L"]
     @Binding var showPizzaOptionScreen: Bool
     @Binding var selectedPizzaOption: String
+    @Binding var optionPrice: Int
     var optionSelected:(String) -> Void
     var body: some View{
         ZStack{
@@ -151,8 +205,9 @@ struct optionPizzaScreen:View{
                 }
                 Form{
                     Picker("", selection: $selectedPizzaOption) {
-                        ForEach(pizzaOption, id: \.self) { option in
-                            Text("\(option)")
+                        ForEach(pizzaOption.sorted(by:{$0.value < $1.value}), id: \.key) { (key,value) in
+                            Text("\(key): \(value)đ")
+                                .font(Font.custom("Bebas Neue", size: 25))
                         }
                     }
                     .pickerStyle(.inline)
@@ -165,15 +220,18 @@ struct optionPizzaScreen:View{
         .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.7)
         .onChange(of: selectedPizzaOption) { newValue in
             optionSelected(newValue)
+            if let price = pizzaOption[newValue]{
+                optionPrice = price
+            }
         }
     }
 }
 
 
 struct optionBurgerScreen:View{
-    let burgerOption = ["Add Vegetable","Add cheese","Add Sauce","No Chili Sauce"]
     @Binding var showBurgerOptionScreen: Bool
     @Binding var selectedBurgerOption: String
+    @Binding var optionPrice: Int
     var optionSelected:(String) -> Void
     var body: some View{
         ZStack{
@@ -182,8 +240,10 @@ struct optionBurgerScreen:View{
             VStack{
                 HStack {
                     Text("Option")
+                        .font(Font.custom("Bebas Neue", size: 30))
                         .foregroundColor(.black)
                         .padding(.leading, 20)
+                        .padding(.top,20)
                         .font(.system(size: 25))
                     Spacer()
                     Button(action: {
@@ -197,8 +257,11 @@ struct optionBurgerScreen:View{
                 }
                 Form{
                     Picker("", selection: $selectedBurgerOption) {
-                        ForEach(burgerOption, id: \.self) { option in
-                            Text("\(option)")
+                        ForEach(burgerOption.sorted(by: { $0.value < $1.value }), id: \.key) { (key, value) in
+                            Group {
+                                Text("\(key): \(value)đ")
+                                    .font(Font.custom("Bebas Neue", size: 25))
+                            }
                         }
                     }
                     .pickerStyle(.inline)
@@ -211,6 +274,22 @@ struct optionBurgerScreen:View{
         .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.7)
         .onChange(of: selectedBurgerOption) { newValue in
             optionSelected(newValue)
+            if let price = burgerOption[newValue]{
+                optionPrice = price
+            }
         }
     }
 }
+
+let burgerOption = ["Add Vegetable": 5000,
+                    "Add cheese": 10000,
+                    "Add Sauce": 0,
+                    "No Chili Sauce":0,
+                    "No option":0,
+]
+
+let pizzaOption = ["No option":0,
+                   "Add meat": 30000,
+                   "Add cheese":20000,
+                   "Upgrade Size M":30000,
+                   "Upgrade Size L":50000]
