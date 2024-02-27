36171275ae1dcbce40cf6f64a93ba1850c074d87
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2024-02-27 19:02:14 +0700

    Update MapView

--- FoodApp/View/MapView.swift
+++ FoodApp/View/MapView.swift
@@ -17,14 +17,50 @@ struct MapView: View {
         longitude: 106.66305
     )
     var body: some View {
-        VStack {
-            MapViewContent(selectedResult: $selectedResult, route: $route, travelTime: $travelTime)
-            if !travelTime.isEmpty {
-                Text("Travel Time: \(travelTime)")
+        ZStack{
+            VStack {
+                MapViewContent(selectedResult: $selectedResult, route: $route, travelTime: $travelTime)
+                
             }
-        }
-        .onAppear {
-            self.selectedResult = MKMapItem(placemark: MKPlacemark(coordinate: self.restaurantLocation))
+            .onAppear {
+                self.selectedResult = MKMapItem(placemark: MKPlacemark(coordinate: self.restaurantLocation))
+            }
+            VStack(alignment: .leading){
+                (Text("The order will be delivered to you within ")
+                    .font(Font.custom("Bebas Neue", size: 20))
+                )
+                +
+                (Text("\(travelTime)")
+                    .font(Font.custom("Bebas Neue", size: 20))
+                    .foregroundColor(Color("AccentColor"))
+                )
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background{
+                Color(.white)
+            }
+            .cornerRadius(10)
+            .padding(.horizontal,10)
+            .offset(y:200)
+            
+            VStack(alignment: .leading){
+                Text("Has Taken food")
+                    .font(Font.custom("Bebas Neue", size: 30))
+                    .padding(.bottom)
+                Text("The driver is delivering the goods to you")
+                    .font(Font.custom("Bebas Neue", size: 20))
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background{
+                Color(.white)
+            }
+            .cornerRadius(10)
+            .padding(.horizontal,10)
+            .offset(y:300)
         }
     }
 }
@@ -40,7 +76,7 @@ struct MapViewContent: View {
     )
     @Binding var selectedResult: MKMapItem?
     @Binding var route: MKRoute?
-@Binding var travelTime: String
+    @Binding var travelTime: String
     var body: some View {
         Map(selection: $selectedResult) {
             Marker("Your Home", coordinate:userLocation)
@@ -55,18 +91,20 @@ struct MapViewContent: View {
             }
         }
         .onChange(of: selectedResult) {
-            getDirection()
+            withAnimation(.easeInOut(duration: 3)) {
+                getDirection()
+            }
         }
     }
-
+    
     func getDirection() {
         self.route = nil
         guard let selectedResult = selectedResult else { return }
-
+        
         let request = MKDirections.Request()
         request.source = MKMapItem(placemark: MKPlacemark(coordinate: userLocation))
         request.destination = selectedResult
-
+        
         Task {
             let direction = MKDirections(request: request)
             let response = try? await direction.calculate()
