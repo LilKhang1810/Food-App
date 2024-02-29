3f7029520b4267b2238bdad2542f3123581c4e0f
LilKhang1810 <95366428+LilKhang1810@users.noreply.github.com>
2024-02-29 10:12:12 +0700

    update 29/2/2024

---
+++ FoodApp/View/AboutAndProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutAndProfileView.swift
+//  FoodApp
+//
+//  Created by Nguyễn Khang Hữu on 29/02/2024.
+//
+
+import SwiftUI
+
+struct AboutAndProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutAndProfileView()
+}
